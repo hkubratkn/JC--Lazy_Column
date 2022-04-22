312a93fc888ae93e567b2050ec8b25362d10962c
CASPER <hkubratkn@gmail.com>
2022-04-22 16:54:01 +0300

    lazycolumn

--- app/src/main/java/com/kapirti/zikirmatik/MainActivity.kt
+++ app/src/main/java/com/kapirti/zikirmatik/MainActivity.kt
@@ -1,30 +1,208 @@
 package com.kapirti.zikirmatik
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.activity.viewModels
 import com.kapirti.zikirmatik.ui.theme.ZikirmatikTheme
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
 
 class MainActivity : ComponentActivity() {
+    val catsViewModel by viewModels<CatsViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ZikirmatikTheme {
-                Button(
-                    onClick={
-                        val intent = Intent()
-                        intent.action=Intent.ACTION_SEND
-                        intent.putExtra(Intent.EXTRA_TEXT,"lalalaa")
-                        intent.type="text/plain"
-                        startActivity(Intent.createChooser(intent, "gegegegeg"))
-                    }
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
                 ){
-                    Text(text="share")
+                    Home(
+                        catsViewModel.cats,
+                        onAddCat = {catsViewModel.addCat(it)},
+                        onRemove = {catsViewModel.removeCat(it)}
+                    )
                 }
             }
         }
     }
+}
+
+class CatsViewModel : ViewModel(){
+    var cats by mutableStateOf(CatsRepo.getCats())
+
+    fun addCat(cat: Cat){
+        cats = cats + listOf(cat)
+    }
+    fun removeCat(cat: Cat){
+        cats = cats.toMutableList().also{
+            it.remove(cat)
+        }
+
+    }
+}
+
+@Composable
+fun Home(
+    cats : List<Cat>,
+    onAddCat : (Cat) -> Unit,
+    onRemove : (Cat) -> Unit
+){
+    Scaffold (
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    onAddCat(generateRandomCats())
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = null
+                )
+            }
+        }
+    ){ innerPadding->
+        LazyColumn(contentPadding = innerPadding) {
+            item {
+                Header(text = "Cats available for adoption")
+            }
+            items(cats) { cat->
+                CatListItem(cat=cat, onRemove= onRemove)
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun CatListItem(cat:Cat, onRemove : (Cat) -> Unit){
+    ListItem(
+        modifier = Modifier.clickable { onRemove(cat) }.padding(8.dp),
+        icon = {
+            Image(
+                painter = painterResource(id = cat.catImage),
+                contentDescription = null,
+                modifier = Modifier.clip(MaterialTheme.shapes.small)
+            )
+        },
+        text = {
+            Text(
+                text = cat.name,
+                style = MaterialTheme.typography.h6
+            )
+        },
+        secondaryText = {
+            Text(
+                text = cat.gender,
+                style = MaterialTheme.typography.body2,
+                color = Color.Black.copy(.5f)
+            )
+        }
+    )
+}
+
+@Composable
+fun Header(
+    text : String,
+    modifier: Modifier = Modifier
+){
+    Surface(
+        color = MaterialTheme.colors.onSurface.copy(.1f),
+        contentColor = MaterialTheme.colors.primary,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    ){
+        Text(
+            text = text,
+            style = MaterialTheme.typography.subtitle2,
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+        )
+    }
+}
+
+
+
+data class Cat(
+    val name: String,
+    val gender: String = "Male",
+    @DrawableRes val catImage: Int,
+)
+
+private val catName = listOf(
+    "Daffy",
+    "Tweety",
+    "Charlie",
+    "Yogi",
+    "Felix",
+    "Shaggy",
+    "Scooby",
+    "Wanda",
+    "Gary",
+    "Oscar",
+    "Elmo",
+    "Bert",
+    "Ernie",
+    "Kermi",
+    "River",
+    "Rocky",
+    "Summit",
+    "Clover",
+    "Aspen",
+    "Birch",
+    "Brook",
+    "Cedar"
+)
+
+private val image = listOf(
+    R.drawable.ic_launcher_background,
+    R.drawable.ic_launcher_foreground,
+    R.drawable.ic_launcher_background,
+    R.drawable.ic_launcher_foreground,
+    R.drawable.ic_launcher_background,
+    R.drawable.ic_launcher_foreground,
+    R.drawable.ic_launcher_background,
+)
+
+private fun cats(): List<Cat> {
+    val cats = mutableListOf<Cat>()
+    for ((index, cat) in catName.withIndex()) {
+        if (index % 2 == 0) {
+            cats.add(Cat(cat, "female", image.random()))
+        } else {
+            cats.add(Cat(cat, catImage = image.random()))
+        }
+    }
+    return cats
+}
+
+object CatsRepo {
+    fun getCats(): List<Cat> = cats()
+}
+
+fun generateRandomCats(): Cat {
+    return cats().random()
 }
--- app/src/main/java/com/kapirti/zikirmatik/screens/CounterScreen.kt
+++ app/src/main/java/com/kapirti/zikirmatik/screens/CounterScreen.kt
@@ -1,60 +1,12 @@
 package com.kapirti.zikirmatik.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import com.kapirti.zikirmatik.ui.theme.ZikirmatikTheme
-
-@Composable
-fun CounterScreenCompos(){
-    ZikirmatikTheme{
-        Surface(
-            color= MaterialTheme.colors.background
-        ){
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ){
-                Text(
-                    text="Counter screen"
-                )
-                Row{
-                    Button(onClick = { /*TODO*/ }) {
-                        Text(text="0")
-                    }
-                    Button(onClick = { /*TODO*/ }) {
-                        Text(text="+")
-                    }
-                }
-            }
-
-        }
-    }
-}
-
-
-/**
- * package com.kapirti.zikirmatik
-
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import android.content.Intent
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.runtime.Composable
@@ -68,145 +20,201 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.kapirti.zikirmatik.R
 import com.kapirti.zikirmatik.ui.theme.ZikirmatikTheme
 
-class MainActivity : ComponentActivity() {
-override fun onCreate(savedInstanceState: Bundle?) {
-super.onCreate(savedInstanceState)
-setContent {
-ZikirmatikTheme {
-NavigationDrawer()
-BodyContent()
-}
-}
-}
+@Composable
+fun CounterScreenCompos(){
+    ZikirmatikTheme{
+        NavigationDrawer()
+        BodyContent()
+    }
 }
 
 @Composable
 fun BodyContent(){
-var navigateClick by remember{ mutableStateOf(false)}
-val offSetAnim by animateDpAsState(targetValue = if (navigateClick) 253.dp else 0.dp)
-val scaleAnim by animateFloatAsState(targetValue = if (navigateClick) 0.6f else 1.0f)
+    var navigateClick by remember{ mutableStateOf(false) }
+    val offSetAnim by animateDpAsState(targetValue = if (navigateClick) 253.dp else 0.dp)
+    val scaleAnim by animateFloatAsState(targetValue = if (navigateClick) 0.6f else 1.0f)
 
-Column(
-modifier = Modifier
-.fillMaxSize()
-.scale(scaleAnim)
-.offset(x = offSetAnim)
-.clip(if (navigateClick) RoundedCornerShape(30.dp) else RoundedCornerShape(0.dp))
-.background(Color.White)
-.verticalScroll(rememberScrollState())
-) {
-Row(
-modifier = Modifier
-.fillMaxWidth()
-.padding(top = 55.dp, start = 45.dp, end = 30.dp)
-){
-Image(
-painter = painterResource(id = R.drawable.ic_launcher_foreground),
-contentDescription ="menu",
-modifier = Modifier
-.clickable { navigateClick = !navigateClick }
-.padding(15.dp)
-)
-}
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .scale(scaleAnim)
+            .offset(x = offSetAnim)
+            .clip(if (navigateClick) RoundedCornerShape(30.dp) else RoundedCornerShape(0.dp))
+            .background(Color.White)
+            .verticalScroll(rememberScrollState())
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 55.dp, start = 45.dp, end = 30.dp)
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription ="menu",
+                modifier = Modifier
+                    .clickable { navigateClick = !navigateClick }
+                    .padding(15.dp)
+            )
+        }
 
-Column(
-modifier = Modifier.fillMaxSize(),
-horizontalAlignment = Alignment.CenterHorizontally,
-verticalArrangement = Arrangement.Center
-){
-Text(
-text ="drawer menu",
-fontWeight = FontWeight.Bold,
-fontFamily = FontFamily.Serif,
-fontSize = 20.sp
-)
-}
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){
+            Text(
+                text ="drawer menu",
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Serif,
+                fontSize = 20.sp
+            )
+        }
+        /** Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .scale(scaleAnim)
+                    .offset(x = offSetAnim)
+                    .clip(if (navigateClick) RoundedCornerShape(30.dp) else RoundedCornerShape(0.dp))
+                    .background(Color.White)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 55.dp, start = 45.dp, end = 30.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                        contentDescription = "menu",
+                        modifier = Modifier
+                            .clickable { navigateClick = !navigateClick }
+                            .padding(15.dp)
+                    )
+                }
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Text(
+                        text="Counter screen"
+                    )
+                    Row{
+                        Button(
+                            onClick = {
 
-}
+                            }
+                        ) {
+                            Text(text="0")
+                        }
+                        Button(
+                            onClick = {
+
+                            }
+                        ) {
+                            Text(text="+")
+                        }
+                    }
+                }
+            }*/
+    }
 }
 
 @Composable
 fun NavigationDrawer(){
-Column(
-modifier = Modifier
-.fillMaxSize()
-.background(MaterialTheme.colors.primary)
-){
-NavigationItem(
-resId =R.drawable.ic_launcher_background,
-text ="profile",
-topPadding = 145.dp
-){}
-NavigationItem(
-resId = R.drawable.ic_launcher_foreground,
-text = "Textttt"
-){}
-
-Row(
-modifier = Modifier
-.padding(start = 50.dp, bottom = 87.dp)
-.fillMaxHeight(),
-horizontalArrangement = Arrangement.spacedBy(12.dp),
-verticalAlignment = Alignment.Bottom
-){
-Text(
-text = "sign out",
-color = Color.White,
-fontSize = 17.sp
-)
-Image(
-imageVector = Icons.Filled.ArrowForward,
-contentDescription = "Logout",
-colorFilter = ColorFilter.tint(Color.White)
-)
-}
-}
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.primary)
+    ){
+        NavigationItem(
+            resId = R.drawable.ic_launcher_background,
+            text = "profile",
+            topPadding = 145.dp
+        ){}
+        NavigationItem(
+            resId = R.drawable.ic_launcher_foreground,
+            text = "Textttt",
+        ){}
+        NavigationItem(
+            resId = R.drawable.ic_launcher_background,
+            text = "sharebtn"
+        ){}
+        Row(
+            modifier = Modifier
+                .padding(start = 50.dp, bottom = 87.dp)
+                .fillMaxHeight(),
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            verticalAlignment = Alignment.Bottom
+        ){
+            val context = LocalContext.current
+            Button(
+                onClick = {
+                    val intent = Intent()
+                    intent.action=Intent.ACTION_SEND
+                    intent.putExtra(Intent.EXTRA_TEXT,"lalalaa")
+                    intent.type="text/plain"
+                    context.startActivity(Intent.createChooser(intent, "gegegegeg"))
+                }
+            ){
+                Text(
+                    text="share",
+                    color = Color.White,
+                    fontSize = 17.sp
+                )
+            }
+            Image(
+                imageVector = Icons.Filled.ArrowForward,
+                contentDescription = "Logout",
+                colorFilter = ColorFilter.tint(Color.White)
+            )
+        }
+    }
 }
 
 @Composable
 fun NavigationItem(
-resId : Int,
-text : String,
-topPadding : Dp = 20.dp,
-destination : String = "",
-itemClicked : (String) -> Unit
+    resId : Int,
+    text : String,
+    topPadding : Dp = 20.dp,
+    destination : String = "",
+    itemClicked : (String) -> Unit
 ){
-Column(
-modifier = Modifier
-.fillMaxWidth()
-.padding(start = 38.dp, top = topPadding)
-.clickable { itemClicked(destination) }
-){
-Row(
-horizontalArrangement = Arrangement.spacedBy(12.dp)
-){
-Image(
-painter = painterResource(id = resId),
-contentDescription = "item image",
-modifier = Modifier.size(30.dp)
-)
-Text(
-text = text,
-color = Color.White,
-fontSize = 16.sp
-)
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 38.dp, top = topPadding)
+            .clickable { itemClicked(destination) }
+    ){
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(12.dp)
+        ){
+            Image(
+                painter = painterResource(id = resId),
+                contentDescription = "item image",
+                modifier = Modifier.size(30.dp)
+            )
+            Text(
+                text = text,
+                color = Color.White,
+                fontSize = 16.sp
+            )
+        }
+        Box(
+            modifier = Modifier
+                .padding(start = 35.dp, top = 26.dp, bottom = 16.dp)
+                .size(120.dp, 0.5.dp)
+                .background(Color.Gray)
+        )
+    }
 }
-
-Box(
-modifier = Modifier
-.padding(start = 35.dp, top = 26.dp, bottom = 16.dp)
-.size(120.dp, 0.5.dp)
-.background(Color.Gray)
-)
-}
-
-}
- * */
